f8e32b25a65ff40c3ceff2c75e882cf551ca9ac1
thelenaza <kaxtie07@gmail.com>
2022-07-04 13:03:29 +0700

    first commit

---
+++ bin/dart_week8.dart
@@ -0,0 +1,12 @@
+void main(){
+  int mark = 80 ;
+  if(mark >= 50 && mark <= 59){ 
+  print("D");
+  }  else if(mark >= 60 && mark <= 69){
+    print ("C");
+  } else if(mark >= 70 && mark <= 79){
+    print ("B");
+  } else if(mark >= 80 && mark <= 100){
+    print("A");
+  }
+}
---
+++ bin/generation.dart
@@ -0,0 +1,19 @@
+void main (){
+  int gen = 1950 ;
+  if(gen >= 2013 && gen <= 2025){
+   print("Gen : Alpha");
+  } else if(gen >= 1997 && gen <= 2012){
+  print("Gen : Z");
+  }  else if(gen >= 1981 && gen <= 1996){                                                                                        
+   print("Gen : Y ");
+  } else if(gen >= 1965 && gen <= 1980){
+   print("Gen : X ");                                                           
+  } else if(gen >= 1946 && gen <= 1964){                       
+   print("Gen : Baby Boomer ");
+  } else if(gen >= 1928 && gen <= 1945){                                                                                        
+    print("Gen : Silent ");                                
+  } else{
+    print("ERROR");
+  }
+  }                                                                                                                                                              
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
